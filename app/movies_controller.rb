771d1522b2102a2a1e23803333e6d8ed1c674e09
Judy McGoldrick <jjeanius@verizon.net>
2018-05-14 02:52:44 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,8 +24,8 @@ end
 
 def can_be_created_in_a_block(arg={})
   Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = 1990
+    m.title = "The Room"
+    m.release_date = 2003
     m.title
   end
 
